0c23870588576748a4788ea4987eb3ed0fdcb3ab
Chris Davies <chris.davies@theironyard.com>
2015-02-05 10:42:05 -0500

    finished mvp, sans styling

--- js/submit.js
+++ js/submit.js
@@ -1,14 +1,19 @@
 $(function () {
+  var userData = $('#user-data').html();
+  var template = _.template(userData, {variable: 'm'});
+
   $('.form').submit(function () {
     var userName = $('.username-input').val();
 
     $.getJSON('https://api.github.com/users/' + userName)
       .done(function (data) {
         console.log(data);
+        $('.wrapper').html(template (data));
       })
       .fail(function (request, status, err) {
         console.log('This failed because ' + err);
       });
     return false;
   });
+
 });
